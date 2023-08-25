a4fbe3476e4afbd0794d728d21bea034e2080892
Syunsuke Ozeki <2dgames.jp@gmail.com>
2023-08-25 11:41:19 +0900

    めくり処理を実装

--- gd_memory/Main.gd
+++ gd_memory/Main.gd
@@ -13,6 +13,9 @@ const GRID_H = 100
 const GRID_CNT_W = 4
 const GRID_CNT_H = 4
 
+## マッチしなかったときの待ち時間.
+const TIMER_WAIT = 1.0
+
 ## 状態.
 enum eState {
 	START, # 開始.
@@ -71,6 +74,23 @@ func _ready() -> void:
 
 ## 更新.
 func _process(delta: float) -> void:
+	# _stateに対応する更新処理を呼び出す.
+	callv("_update_" + eState.keys()[_state], [delta])
+	
+	# デバッグの更新.
+	_update_debug()
+
+## 更新 > 開始.
+func _update_START(delta:float) -> void:
+	_state = eState.MAIN
+## 更新 > メイン.
+func _update_MAIN(delta:float) -> void:
+	if _front_cards.size() >= 2:
+		_state = eState.WAIT
+		_timer = 0
+		return
+	
+	# めくり判定.
 	if Input.is_action_just_pressed("click"):
 		for card in _card_layer.get_children():
 			var c = card as Card
@@ -80,17 +100,42 @@ func _process(delta: float) -> void:
 				_front_cards.append(c)
 				break
 	
-	_check_erase()
 	
-	if Input.is_action_just_pressed("ui_accept"):
-		for card in _front_cards:
-			var c = card as Card
-			c.flip_to_back()
 	
-	_update_debug()
+# 更新 > めくった後の待ち時間.
+func _update_WAIT(delta:float) -> void:
+	for card in _front_cards:
+		var c = card as Card
+		if c.is_front == false:
+			return # すべてが表向きになるまで待つ.
+
+	# さらに待つ.
+	_timer += delta
+	if _timer > TIMER_WAIT:
+		if _check_erase():
+			# 消せたのでめくったカード情報を消す.
+			_front_cards.clear()
+		else:
+			# カードをもとに戻す.
+			for card in _front_cards:
+				var c = card as Card
+				c.flip_to_back()
+			# めくったカード情報を消す.
+			_front_cards.clear()
+		_state = eState.MAIN
+		_timer = 0
+
+# 更新 > ゲームオーバー.
+func _update_GAMEOVER(delta:float) -> void:
+	pass
+
+# 更新 > ゲームクリア
+func _update_GAMECLEAR(delta:float) -> void:
+	pass
 
 ## 消去チェック.
-func _check_erase() -> void:
+func _check_erase() -> bool:
+	var ret = false # マッチしたカードがあるかどうか.
 	var list = []
 	for i in range(Card.eId.size()):
 		list.append(0)
@@ -101,18 +146,24 @@ func _check_erase() -> void:
 		# カウントだけする.
 		list[c.id] += 1
 	
+	print(list)
+	
 	for idx in range(list.size()):
 		if list[idx] < 2:
 			continue
 		
+		# マッチしたカードがある.
+		ret = true
+		
+		# マッチしたカードを消す.
 		for card in _card_layer.get_children():
 			var c = card as Card
 			if c.is_front == false:
 				continue # 対象カードは表のものだけ.
 			if c.id == idx:
 				c.vanish()
-			
-
+	
+	return ret
 
 func _update_debug() -> void:
 	if Input.is_action_just_pressed("reset"):
